b9b0b173eadab6bc5561e8de8040e9f4af79c0ea
Daniil Aleksieiev <daniel.aleksieiev@gmail.com>
2020-02-14 13:55:48 +0200

    initial commit

---
+++ precache-manifest.b992c570a2b09424a484e4e1315259c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e28c4b5112b647676c6e48adc37719",
+    "url": "/index.html"
+  },
+  {
+    "revision": "570e381f245550208ce4",
+    "url": "/static/css/main.fa452856.chunk.css"
+  },
+  {
+    "revision": "8e9a0230f32f7d36edfe",
+    "url": "/static/js/2.3c67d9ac.chunk.js"
+  },
+  {
+    "revision": "21f38193c3caf91fe32157540d33cdde",
+    "url": "/static/js/2.3c67d9ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "570e381f245550208ce4",
+    "url": "/static/js/main.4c93104a.chunk.js"
+  },
+  {
+    "revision": "9e589b1b39880b37acb9",
+    "url": "/static/js/runtime-main.c78ac275.js"
+  }
+]);
