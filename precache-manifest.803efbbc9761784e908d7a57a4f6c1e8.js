107057736658a2de0398037c16e06e8588e4b0d6
Daniil Aleksieiev <daniel.aleksieiev@gmail.com>
2020-03-02 15:35:45 +0200

    cv updated

--- precache-manifest.803efbbc9761784e908d7a57a4f6c1e8.js
+++ precache-manifest.803efbbc9761784e908d7a57a4f6c1e8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "94621893e3a4adfad78f2c747902eed8",
+    "revision": "2577cdad29a22db0bbade0900dc155dd",
     "url": "/index.html"
   },
   {
-    "revision": "4aa75df8f416bc005911",
+    "revision": "b7e3a9df80f3728b94fe",
     "url": "/static/css/main.4a279294.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.bee501c7.chunk.js.LICENSE"
   },
   {
-    "revision": "4aa75df8f416bc005911",
-    "url": "/static/js/main.70733792.chunk.js"
+    "revision": "b7e3a9df80f3728b94fe",
+    "url": "/static/js/main.8331dc66.chunk.js"
   },
   {
     "revision": "9e589b1b39880b37acb9",
